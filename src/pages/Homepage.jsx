d26afc472833939f4cfae146736bd016d21a046d
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-01-20 13:35:13 +0100

    create SPA worldwise

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Wrildswise</h1>
+      <PageNav />
+    </div>
+  );
+}
+
+export default Homepage;
