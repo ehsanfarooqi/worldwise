4d15c6d1a83b6dd9d3083071dfdb2b679a51a0c1
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-01-28 15:07:48 +0100

    useGeolocation map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,23 +9,43 @@ import {
 } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
-
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 import styles from "./Map.module.css";
 
 function Map() {
+  const { cities } = useCities();
   const [mapPosition, setMapPostion] = useState([40, 0]);
   const [searchParms, setSearchParms] = useSearchParams();
-  const { cities } = useCities();
+  const {
+    isLoading: isLoadingPostion,
+    position: geoloacationPotion,
+    getPosition,
+  } = useGeolocation();
 
   const mapLat = searchParms.get("lat");
   const mapLng = searchParms.get("lng");
 
+  // Change center position effect
   useEffect(() => {
     if (mapLat && mapLng) setMapPostion([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  // Get your current postion
+  useEffect(() => {
+    if (geoloacationPotion)
+      setMapPostion([geoloacationPotion.lat, geoloacationPotion.lng]);
+  }, [geoloacationPotion]);
+
   return (
     <div className={styles.mapContainer}>
+      // Buttun get your current postion
+      {!geoloacationPotion && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPostion ? "Loading..." : "use your loaction"}
+        </Button>
+      )}
+      // React components for Leaflet maps
       <MapContainer
         className={styles.map}
         center={mapPosition}
@@ -53,12 +73,14 @@ function Map() {
   );
 }
 
+// Change center position Component
 function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
 }
 
+// Deteckt or MapEvent Component
 function DetectClick() {
   const navigate = useNavigate();
 
